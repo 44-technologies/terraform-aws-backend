697948663fa0d7ba0901c386580a1b1a50e8973e
44-technologies <alberto@44t.tech>
2026-02-22 17:36:18 +0100

    Bump Versions

--- tests/setup/versions.tf
+++ tests/setup/versions.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = "3.8.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.62.0"
+      version = ">= 6.33.0"
     }
   }
 
-  required_version = ">= 1.9.4"
+  required_version = ">= 1.14.5"
 }
-
--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 5.80.0"
+      version               = ">= 6.33.0"
       configuration_aliases = [aws.backend]
     }
   }
 
-  required_version = ">= 1.10.1"
+  required_version = ">= 1.14.5"
 }
 

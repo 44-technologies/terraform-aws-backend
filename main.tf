0d46b231db694c2978402a78c2d4c5b82b067cab
alberto.hernandez <alberto@44t.tech>
2026-05-03 16:43:34 +0100

    Remove Dynamo Table to Control Lock

--- main.tf
+++ main.tf
@@ -48,16 +48,3 @@ resource "aws_s3_bucket_public_access_block" "bucket_state_public_block" {
   restrict_public_buckets = true
 }
 
-
-# Lock Table
-resource "aws_dynamodb_table" "terraform_lock" {
-  provider = aws.backend
-
-  name         = var.bucket_lock_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 6.33.0"
+      version               = ">= 6.43.0"
       configuration_aliases = [aws.backend]
     }
   }
 
-  required_version = ">= 1.14.5"
+  required_version = ">= 1.15.1"
 }
 
